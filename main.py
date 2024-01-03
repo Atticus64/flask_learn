326d726995fd2ccedff6e8533adcf42efabc7c7d
Jonathan <jonathanelian64@gmail.com>
2024-01-03 17:54:26 -0600

    fix: port env variable

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from controllers import user
 
 app = Flask(__name__)
 
+port = int(os.environ.get("PORT", 5000))
+
 app.add_url_rule("/logout", view_func=user.logout)
 app.add_url_rule("/", view_func=user.hello_cat)
 app.add_url_rule("/json", view_func=user.json)
@@ -10,3 +12,5 @@ app.add_url_rule("/<name>", view_func=user.greet)
 app.add_url_rule("/movies/create", view_func=user.create_movie, methods=['POST'])
 app.add_url_rule("/movies", view_func=user.get_movies)
 
+if __name__ == "__main__":
+    app.run(port=port)
